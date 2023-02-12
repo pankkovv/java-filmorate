705e4998efd429ea62f5eabe4077a3546f6f1756
Панков Денис <Pankkovv@yandex.ru>
2023-02-12 12:31:39 +0300

    feat: добавить валидацию полей класса

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,15 +1,34 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
+import ru.yandex.practicum.filmorate.exception.PostureDate;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @Builder
 public class Film {
     private int id;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Size(max = 200)
     private String description;
+    @NotNull
+    @PostureDate
     private LocalDate releaseDate;
+    @NotNull
+    @Positive
     private Integer duration;
+    private Set<Integer> likes;
+
+    public void addLikes(Integer userId) {
+        likes.add(userId);
+    }
+
+    public void removeLikes(Integer userId) {
+        likes.remove(userId);
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,14 +2,30 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @Builder
 public class User {
     private int id;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
     private String login;
     private String name;
+    @NotNull
+    @PastOrPresent
     private LocalDate birthday;
+    private Set<Integer> friends;
+
+    public void addFriends(int userId) {
+        friends.add(userId);
+    }
+
+    public void removeFriends(int userId) {
+        friends.remove(userId);
+    }
 }
