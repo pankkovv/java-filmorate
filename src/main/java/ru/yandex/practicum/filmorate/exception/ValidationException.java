0574ae8c7722bcffd91bda6486d112ed13fd131b
Панков Денис <Pankkovv@yandex.ru>
2023-02-12 12:36:26 +0300

    feat: создать класс исключение

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,16 +1,20 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ValidationException extends Throwable {
     private String message;
 
-    public ValidationException(){};
+    public ValidationException() {
+    }
 
-    public ValidationException(String message){
+    public ValidationException(String message) {
         this.message = message;
-    };
+    }
 
     @Override
-    public String getMessage(){
+    public String getMessage() {
         return message;
     }
 }
